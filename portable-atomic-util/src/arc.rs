8491e5df81b3c08113ab5cfbe88103784c37a162
@brodycj - C. Jonathan Brody <cj@brodycj.com>
2024-11-20 13:31:39 -0500

    Update portable-atomic-util/src/arc.rs - remove "(with Rust nightly)"

--- portable-atomic-util/src/arc.rs
+++ portable-atomic-util/src/arc.rs
@@ -2302,7 +2302,7 @@ impl<T, const N: usize> From<[T; N]> for Arc<[T]> {
     /// ```
     #[inline]
     fn from(v: [T; N]) -> Self {
-        // Casting Arc<[T; N]> -> Arc<[T]> requires unstable CoerceUnsized (with Rust nightly),
+        // Casting Arc<[T; N]> -> Arc<[T]> requires unstable CoerceUnsized,
         // so we convert via Box to support stable & beta Rust channels.
         // Since the compiler knows the actual size and metadata, the intermediate allocation is
         // optimized and generates the same code as when using CoerceUnsized and convert Arc<[T; N]> to Arc<[T]>.
